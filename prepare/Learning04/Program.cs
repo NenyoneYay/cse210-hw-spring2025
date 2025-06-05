e22f466c599dc5f2e46f77955faf467291b22dbf
unknown <mikemaster98@gmail.com>
2025-06-04 18:41:30 -0600

    What a terrible night to have a curse

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.Clear();
+        Assignment testAssignment = new Assignment("Bob Testingburger", "TopicTest");
+
+        MathAssignment mathAttack = new MathAssignment("Bob Testingburger", "Math", "The Bible", "How many woodchucks can YOU sit on?");
+        string mathAttackString = mathAttack.GetHomeworkList();
+        Console.WriteLine(mathAttack.GetSummary());
+        Console.WriteLine(mathAttackString);
+        WritingAssignment writeBooks = new WritingAssignment("Stacy Bookwriter", "Writing", "Once upon a midnight hour, Stacy tasted something sour.");
+        Console.WriteLine(writeBooks.GetSummary() + "\n" + writeBooks.GetWritingInformation());
     }
 }
