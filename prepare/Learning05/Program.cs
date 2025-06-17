1e775a5e95527cb335506807d93a38b7fcd86d07
unknown <mikemaster98@gmail.com>
2025-06-17 01:28:33 -0600

    squares

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,22 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Rectangle myRectangle = new Rectangle("Red", 10, 8);
+        // mySquare.SetColor("blue");
+        Console.WriteLine("Testing: " + myRectangle.GetColor());
+        Console.WriteLine("Area: " + myRectangle.GetArea());
+
+        Circle myCircle = new Circle("purple", 7);
+        // mySquare.SetColor("blue");
+        Console.WriteLine("Testing: " + myCircle.GetColor());
+        Console.WriteLine("Area: " + myCircle.GetArea());
+
+        Square mySquare = new Square("silver", 4);
+        // mySquare.SetColor("blue");
+        Console.WriteLine("Testing: " + mySquare.GetColor());
+        Console.WriteLine("Area: " + mySquare.GetArea());
     }
+
 }
